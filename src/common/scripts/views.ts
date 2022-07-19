ae12e66637e4734439fc62160c251bebb4f1205b
User <user@keeweb.local>
2022-07-19 15:05:48 +0500

    feat(proxy): add public proxy iteration

--- src/common/scripts/views.ts
+++ src/common/scripts/views.ts
@@ -1,10 +1,9 @@
 import { HttpService } from '@nestjs/axios';
-import { HttpException } from '@nestjs/common';
-import { catchError } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 import { ConfigService } from '@nestjs/config';
 import { HttpsProxyAgent } from 'https-proxy-agent';
-
-// import {workerData} from 'worker_threads'
+import { performance } from 'perf_hooks';
+import Axios from 'axios';
 
 const viewsWorker = async (
   url: string,
@@ -12,97 +11,49 @@ const viewsWorker = async (
   httpService: HttpService,
   configService: ConfigService,
 ) => {
-  const proxys = await httpService
-    .get(
+  const proxys = await firstValueFrom(
+    httpService.get(
       'https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt',
-    )
-    .pipe(
-      catchError((e) => {
-        throw new HttpException(e.response.data, e.response.status);
+    ),
+  );
+  const apis = proxys.data.split(/\r?\n/);
+
+  const successApi = [];
+
+  for await (const api of apis) {
+    if (successApi.length === count) break;
+
+    const [host, port] = api.split(':');
+
+    const agent = new HttpsProxyAgent(`http://${host}:${port}`);
+    // const API_PROXYS = configService.get('API_PROXYS');
+    // const agent = new HttpsProxyAgent(`http://${username}:${password}@${host}:${port}`);
+
+    const start = performance.now();
+
+    const cancelToken = Axios.CancelToken.source();
+
+    setTimeout(() => {
+      cancelToken.cancel();
+    }, 2000);
+
+    await firstValueFrom(
+      httpService.get(url, {
+        httpsAgent: agent,
+        cancelToken: cancelToken.token,
       }),
     )
-    .toPromise();
+      .then(() => {
+        console.log('ok', host);
+        successApi.push(`${host}:${port}`);
+      })
+      .catch(() => {
+        console.log('no ok', host);
+      })
+      .finally(() => console.log(performance.now() - start));
+  }
 
-  const apis = proxys.data.split(/\r?\n/).slice(0, 100);
-
-  const apisList = await Promise.all(
-    apis.map(async (api) => {
-      const [host, port] = api.split(':');
-      const agent = await new HttpsProxyAgent(`http://${host}:${port}`);
-
-      try {
-        await httpService
-          .get(url, {
-            httpsAgent: agent,
-          })
-          .pipe(
-            catchError((e) => {
-              throw new HttpException(e.response.data, e.response.status);
-            }),
-          )
-          .toPromise();
-
-        console.log(api);
-
-        return Promise.resolve(api);
-      } catch (_) {
-        console.log('error ', api);
-        return Promise.resolve(null);
-      }
-    }),
-  );
-
-  console.log(apisList.filter(Boolean), apisList.filter(Boolean).length);
-  // const API_PROXYS = configService.get('API_PROXYS');
-  // const {
-  //   data: { results: proxys },
-  // } = await httpService
-  //   .get(API_PROXYS)
-  //   .pipe(
-  //     catchError((e) => {
-  //       console.log(e);
-  //       throw new HttpException(e.response.data, e.response.status);
-  //     }),
-  //   )
-  //   .toPromise();
-
-  // console.log(
-  //   await Promise.all(
-  //     proxys.map(async ({ ip: host, username, password, port_http: port }) => {
-  // const agent = new HttpsProxyAgent(
-  //   `http://${username}:${password}@${host}:${port}`,
-  // );
-
-  //       try {
-  //         await httpService.get(url, { httpsAgent: agent }).pipe(
-  //           catchError((e) => {
-  //             throw new HttpException(e.response.data, e.response.status);
-  //           }),
-  //         );
-
-  //         return Promise.resolve(true);
-  //       } catch (_) {
-  //         return Promise.resolve(null);
-  //       }
-  //     }),
-  //   ),
-  // );
-  // const browser = await p.launch({
-  //   args: [
-  //     '--proxy-server=http://46.150.252.20:45214',
-  //     '--no-sandbox',
-  //     '--disable-setuid-sandbox',
-  //   ],
-  // });
-  // const page = await browser.newPage();
-  // await page.authenticate({
-  //   username: 'mQrAjEdg',
-  //   password: '5sqt4VCY',
-  // });
-  // await page.goto(url);
-  // await page.waitForNavigation({ waitUntil: 'load' });
-  // await page.waitForTimeout(1000);
-  // await page.close();
+  console.log(successApi);
 };
 
 export default viewsWorker;
